dafbfc992090a228fe8084c81a4058201721cd01
Diego Fernando <mccallotorcillas@gmail.com>
2025-08-28 15:34:56 -0500

    preparar version 1.0.0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
-//Este es un metodoooooo
+//Este es un metodoooooo en am code a seguir esforzando 
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
